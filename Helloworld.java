0b4ca16260427b19ccd1555785ef572e05ca685a
qudusakintunde <akintundequdus29@gmail.com>
2025-12-17 09:10:40 +0000

    Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+  public static void main(String[]args){
+    system.out.println("Hello,world!");
+    system.out.println("My name is Akintunde Qudus.");
+    system.out.println("I an studying Computer SCience in the UK.");
+  }
+}
+      
